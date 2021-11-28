eba847573969a1d013675d3b347656d23dba4eac
David Mansilla <davidjamirmansilla@gmail.com>
2021-11-28 17:43:33 -0300

    MS-03: re design table to list candidates

--- src/Components/Candidates/index.js
+++ src/Components/Candidates/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from './candidates.module.css';
-import Form from './Form';
-import List from './List';
+/*import Form from './Form';
+import List from './List';*/
 
 function Candid() {
   const [candidates, setCandidates] = useState([]);
@@ -10,16 +10,47 @@ function Candid() {
       .then((response) => response.json())
       .then((response) => {
         setCandidates(response.candidates);
+      })
+      .catch((err) => {
+        console.log(err);
       });
   }, []);
-  console.log(candidates);
 
   return (
     <section className={styles.container}>
       <h2>Candidates</h2>
       <div>
-        <Form />
-        <List />
+        <table className={styles.resourceTable}>
+          <thead>
+            <tr className={styles.table}>
+              <th className={styles.th}>firstName</th>
+              <th className={styles.th}>lastName</th>
+              <th className={styles.th}>email</th>
+              <th className={styles.th}>password</th>
+              <th className={styles.th}>phone</th>
+              <th className={styles.th}>Actions</th>
+            </tr>
+          </thead>
+        </table>
+        {candidates.map((candidate) => {
+          return (
+            <table className={styles.resourceTable} key={candidate.id}>
+              <tbody className={styles.table}>
+                <tr>
+                  <td className={styles.td}>{candidate.firstName}</td>
+                  <td className={styles.td}>{candidate.lastName}</td>
+                  <td className={styles.td}>{candidate.email}</td>
+                  <td className={styles.td}>{candidate.password}</td>
+                  <td className={styles.td}>{candidate.phone}</td>
+                  <td>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          );
+        })}
       </div>
     </section>
   );
