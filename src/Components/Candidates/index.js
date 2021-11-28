ad9b9d1364410bfab98f88a36b52a59a33071972
David Mansilla <davidjamirmansilla@gmail.com>
2021-11-28 18:17:23 -0300

    MS-03: add function to delete a candidate

--- src/Components/Candidates/index.js
+++ src/Components/Candidates/index.js
@@ -20,7 +20,8 @@ function Candid() {
     <section className={styles.container}>
       <h2>Candidates</h2>
       <div>
-        <table className={styles.resourceTable}>
+        <button>Create</button>
+        <table className={styles.resourceTableHead}>
           <thead>
             <tr className={styles.table}>
               <th className={styles.th}>firstName</th>
@@ -34,17 +35,58 @@ function Candid() {
         </table>
         {candidates.map((candidate) => {
           return (
-            <table className={styles.resourceTable} key={candidate.id}>
+            // eslint-disable-next-line react/jsx-key
+            <table className={styles.resourceTable}>
               <tbody className={styles.table}>
                 <tr>
-                  <td className={styles.td}>{candidate.firstName}</td>
+                  <td className={styles.td} key={candidate._id}>
+                    {candidate.firstName}
+                  </td>
                   <td className={styles.td}>{candidate.lastName}</td>
                   <td className={styles.td}>{candidate.email}</td>
                   <td className={styles.td}>{candidate.password}</td>
                   <td className={styles.td}>{candidate.phone}</td>
+                  <td className={styles.td}>{candidate.phone}</td>
+                  <td className={styles.td}>{candidate.city}</td>
+                  <td className={styles.td}>{candidate.province}</td>
+                  <td className={styles.td}>{candidate.country}</td>
+                  <td className={styles.td}>{candidate.postalCode}</td>
+                  <td className={styles.td}>{candidate.birthday}</td>
+                  <td className={styles.td}>{candidate.hobbies}</td>
+                  <td className={styles.td}>{candidate.mainSkills}</td>
+                  <td className={styles.td}>{candidate.profileTypes}</td>
+                  <td className={styles.td}>{candidate.isOpenToWork}</td>
+                  <td className={styles.td}>{candidate.isActive}</td>
+                  <td className={styles.td}>{candidate.education}</td>
+                  <td className={styles.td}>{candidate.experiences}</td>
+                  <td className={styles.td}>{candidate.courses}</td>
                   <td>
                     <button>Edit</button>
-                    <button>Delete</button>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        fetch(`${process.env.REACT_APP_API}/candidates/${candidate._id}`, {
+                          method: 'DELETE',
+                          headers: {
+                            'Content-type': 'application/json; charset=UTF-8'
+                          }
+                        })
+                          .then((response) => {
+                            if (response.status == 204) {
+                              setCandidates(
+                                candidates.filter(
+                                  (deleteCandidate) => deleteCandidate._id !== candidate._id
+                                )
+                              );
+                            }
+                          })
+                          // eslint-disable-next-line
+                          .then((res) => location.reload())
+                          .catch((error) => console.log(error));
+                      }}
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
               </tbody>
