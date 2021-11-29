863fc96d4e0e3f3b8b5a725f0a67f4b92273a510
Baltazar Cortina <baltazarcortina@gmail.com>
2021-11-29 09:31:47 -0300

    MS-06: Add Fieldset component

---
+++ src/Components/Positions/Fieldset/index.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react';
+import Options from '../Options';
+import { capitalize } from '../helpers';
+
+// import styles from './fieldset.module.css';
+
+function Fieldset({
+  operation,
+  currentId,
+  element,
+  resource,
+  name,
+  resourceName,
+  type,
+  required,
+  updateData
+}) {
+  const [inputValue, setInputValue] = useState('');
+
+  const changeInputValue = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+    updateData(resourceName, value);
+  };
+
+  useEffect(() => {
+    if (operation === 'update' && !inputValue) {
+      setInputValue(currentId);
+      updateData(resourceName, currentId);
+    }
+  });
+
+  let field;
+  switch (element) {
+    case 'select':
+      field = (
+        <select
+          name={name}
+          id={name}
+          value={inputValue}
+          required={required}
+          onChange={changeInputValue}
+        >
+          <Options name={name} resource={resource} operation={operation} />
+        </select>
+      );
+      break;
+    case 'input':
+      field = (
+        <input
+          type={type}
+          name={name}
+          id={name}
+          value={inputValue}
+          required={required}
+          onChange={changeInputValue}
+        ></input>
+      );
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <fieldset>
+      <label htmlFor={name}>{capitalize(name)}</label>
+      {field}
+    </fieldset>
+  );
+}
+
+export default Fieldset;
