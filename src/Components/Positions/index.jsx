40d40d0e4c0dada14f275ff556319547d2c7b542
Baltazar Cortina <baltazarcortina@gmail.com>
2021-11-29 08:46:48 -0300

    MS-06: Add basic structure to Positions component

---
+++ src/Components/Positions/index.jsx
@@ -0,0 +1,21 @@
+import styles from './positions.module.css';
+
+function Positions() {
+  const addPosition = () => {
+    localStorage.setItem('operation', 'create');
+    localStorage.setItem('id', '');
+    window.location.pathname = './positions/form';
+  };
+
+  return (
+    <section className={styles.container}>
+      <h2>Positions</h2>
+      <List />
+      <button className={styles.createBtn} onClick={addPosition}>
+        ADD POSITION
+      </button>
+    </section>
+  );
+}
+
+export default Positions;
