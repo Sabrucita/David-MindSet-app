674d7848fc2211decaf652355b4346de047d3230
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-12-01 22:10:19 -0300

    Modal code added

---
+++ src/Components/Psychologists/Modal/index.js
@@ -0,0 +1,89 @@
+import styles from './modal.module.css';
+import React from 'react';
+import ListItem from '../ListItem';
+
+export const Modal = (props) => {
+  if (!props.show) {
+    return null;
+    //MODAL FOR DELETE CONFIRMATION
+  } else if (props.action == 'delete') {
+    const onConfirmDeleteModal = () => {
+      props.onCloseModal();
+      props.closeModal();
+    };
+    const onReturnModal = () => {
+      props.closeModal();
+    };
+
+    return (
+      <div className={styles.modal}>
+        <div className={styles.centerModal}>
+          <h3 className={styles.modalMessage}>Are you sure you want to delete this psycho?</h3>
+          <div className={styles.buttonModal}>
+            <button className={styles.modalOk} onClick={onConfirmDeleteModal}>
+              DELETE
+            </button>
+            <button className={styles.modalCancel} onClick={onReturnModal}>
+              RETURN
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+    //MODAL FOR VIEWING AN ITEM LIST
+  } else if (props.action == 'view') {
+    const onViewItemModal = () => {
+      props.closeModal();
+    };
+    return (
+      <div className={styles.modal}>
+        <div className={styles.centerModal}>
+          <div className={styles.modalMessage}>
+            <ListItem className={styles.modalMessage} itemListInfo={props.itemListInfo} />;
+          </div>
+          <div className={styles.buttonModal}>
+            <button className={styles.modalCancel} onClick={onViewItemModal}>
+              RETURN
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  } else if (props.action == 'update') {
+    const onUpdModal = () => {
+      window.location.href = '/psychologists';
+    };
+    return (
+      <div className={styles.modal}>
+        <div className={styles.centerModal}>
+          <div className={styles.modalMessage}>
+            <h3 className={styles.modalMessage}>Your psychologist has been updated succesfully</h3>
+          </div>
+          <div className={styles.buttonModal}>
+            <button className={styles.modalCancel} onClick={onUpdModal}>
+              RETURN TO HOMEPAGE
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  } else if (props.action == 'create') {
+    const onUpdModal = () => {
+      window.location.href = '/psychologists';
+    };
+    return (
+      <div className={styles.modal}>
+        <div className={styles.centerModal}>
+          <div className={styles.modalMessage}>
+            <h3 className={styles.modalMessage}>Your psychologist has been created succesfully</h3>
+          </div>
+          <div className={styles.buttonModal}>
+            <button className={styles.modalCancel} onClick={onUpdModal}>
+              RETURN TO HOMEPAGE
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
