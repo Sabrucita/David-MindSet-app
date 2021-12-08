f99fa6b0c0cbd71c3f28548a5c6d6660293f56a4
Eugenio Ponzone <eugenioponzone@gmail.com>
2021-12-08 20:27:23 -0300

    MS-26: add redux dependencies and create basic structure

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './Components/Layout';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './redux/reducers';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Layout />
+    <Provider store={store}>
+      <Layout />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
