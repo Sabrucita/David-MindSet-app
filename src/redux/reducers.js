5e8bf07eee5f718d17b342223cf15f48baf3edb1
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-12-11 00:17:45 -0300

    MS-27: Added adminsReducer to Reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,8 +1,10 @@
 import applicationReducer from './applications/reducer';
+import adminsReducer from './admins/reducer';
 import { combineReducers } from 'redux';
 
 const reducer = combineReducers({
-  application: applicationReducer
+  application: applicationReducer,
+  admins: adminsReducer
 });
 
 export default reducer;
