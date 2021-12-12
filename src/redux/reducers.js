0024bfe3830834ff15b0033ce161707a0233dcb9
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-12-12 20:29:15 -0300

    MS-34: Modal and Psychologists reducers added to general reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,8 +1,12 @@
 import applicationReducer from './applications/reducer';
+import psychologistsReducer from './psychologists/reducer';
+import modalReducer from './modal/reducer';
 import { combineReducers } from 'redux';
 
 const reducer = combineReducers({
-  application: applicationReducer
+  application: applicationReducer,
+  psychologists: psychologistsReducer,
+  modal: modalReducer
 });
 
 export default reducer;
