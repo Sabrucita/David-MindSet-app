8777cd80175c89c8f602b1753d82d177b220f8ef
David Mansilla <davidjamirmansilla@gmail.com>
2021-12-13 03:57:07 -0300

    MS-33: things that i forgot to save

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,8 +1,12 @@
 import applicationReducer from './applications/reducer';
+import profileReducer from './profiles/reducer';
+import modalReducer from './modal/reducer';
 import { combineReducers } from 'redux';
 
 const reducer = combineReducers({
-  application: applicationReducer
+  application: applicationReducer,
+  profile: profileReducer,
+  modal: modalReducer
 });
 
 export default reducer;
