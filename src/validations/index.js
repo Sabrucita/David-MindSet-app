24081c9fab4112646af35557fd97a5280a24238b
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-12-18 14:16:16 -0300

    MS-51: Text, Password and email validations added

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,2 +1,33 @@
 // SOLAMENTE VALIDACIONES COMPARTIDAS
 // AVISAR POR WP / SLACK CUANDO SE AGREGUE ALGUNA!!!! O HAY TABLA
+
+export const pastDatesValidation = (date) => {
+  const today = new Date();
+  date = new Date(date);
+  if (date < today) {
+    return `Date must be later than today's date`;
+  }
+  return undefined;
+};
+
+export function validateText(value, field, min, max) {
+  let error = {};
+  if (!value) {
+    error = `${field} is required`;
+  } else if (max && value.length > max) {
+    error = `${field} must be less than ${max} characters`;
+  } else if (min && value.length < min) {
+    error = `${field} must be more than ${min} characters`;
+  } else {
+    return undefined;
+  }
+  return error;
+}
+
+export function emailValidationFn(errors, formValues) {
+  const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/;
+  const emailValidation = formValues.email.search(emailRegex);
+  if (emailValidation != 0) {
+    errors.email = 'Please enter a valid email address';
+  }
+}
