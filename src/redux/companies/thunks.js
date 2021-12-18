0098e15ab060f3c91f48661945552c9bfc6ed28b
Sabrina <pereira.tsabrina@gmail.com>
2021-12-18 19:59:40 -0300

    MS-36: console.log deleted

--- src/redux/companies/thunks.js
+++ src/redux/companies/thunks.js
@@ -28,7 +28,6 @@ export const getCompany = (id) => {
       .then(async (res) => {
         if (res.status === 200) {
           const data = await res.json();
-          console.log(data);
           const currentData = {
             name: data.name,
             address: data.address,
@@ -138,7 +137,6 @@ export const createCompany = (company) => {
 //UPDATE COMPANY
 
 export const updateCompany = (id, company) => {
-  console.log(company);
   return (dispatch) => {
     dispatch(updateCompanyFetching());
     dispatch(showModal('companies', 'fetching'));
