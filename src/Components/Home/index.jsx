59177d17c9b5a2061963864545344adce3d2dc3a
Baltazar Cortina <baltazarcortina@gmail.com>
2021-12-18 23:09:01 -0300

    MS-46: Add public route

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,13 +1,10 @@
-import Layout from 'Components/Layout';
 import styles from './home.module.css';
 
 function Home() {
   return (
-    <Layout resource={'admin'}>
-      <section className={styles.container}>
-        <h2>Home</h2>
-      </section>
-    </Layout>
+    <section className={styles.container}>
+      <h2>Home</h2>
+    </section>
   );
 }
 
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,15 +1,15 @@
 import { lazy, Suspense } from 'react';
 import { Switch, Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
-import Home from 'Components/Home/index';
 const AdminRoutes = lazy(() => import('Routes/admin'));
 const CandidateRoutes = lazy(() => import('Routes/candidate'));
+const PublicRoutes = lazy(() => import('Routes/public'));
 
 const Routes = () => {
   return (
     <Router>
       <Suspense fallback={<div />}>
         <Switch>
-          <Route path="/home" component={Home} />
+          <Route path="/home" component={PublicRoutes} />
           <Route path="/candidate" component={CandidateRoutes} />
           <Route path="/admin" component={AdminRoutes} />
           <Redirect to="/home" />
