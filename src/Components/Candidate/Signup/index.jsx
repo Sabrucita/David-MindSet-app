cde2610a1ca84397f35f5751ed0c497116b12185
David Mansilla <davidjamirmansilla@gmail.com>
2021-12-20 20:55:03 -0300

    MS-38: add signups steps

--- src/Components/Candidate/Signup/index.jsx
+++ src/Components/Candidate/Signup/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Fieldset from 'Components/shared/Fieldset';
 import styles from './signup.module.css';
 import Modal from 'Components/shared/Modal';
@@ -6,12 +6,30 @@ import { useDispatch, useSelector } from 'react-redux';
 import { createCandidates } from 'redux/admin/candidates/thunks';
 import { candidatesCleanUp } from 'redux/admin/candidates/actions';
 import { Form, Field } from 'react-final-form';
-import { validateEmail, validateText } from 'validations';
+import {
+  validateText,
+  validatePhone,
+  validateEmail,
+  birthdayValidation,
+  validateZipCode
+} from 'validations';
+import SignUp1 from './signup1';
+import SignUp2 from './signup2';
 
-function SignUp() {
+function SignUp({ match }) {
   const dispatch = useDispatch();
   const formData = useSelector((store) => store.candidates.selectedElement);
   const modal = useSelector((store) => store.modal.show);
+  const FormTitle = ['Sign Up', 'Sign Up2'];
+  const [page, setPage] = useState(0);
+  const PageDisplay = () => {
+    if (page === 0) {
+      return <SignUp1 />;
+    } else if (page === 1) {
+      return <SignUp2 />;
+    }
+  };
+  const id = match.params.id;
 
   useEffect(() => {
     return () => {
@@ -20,58 +38,55 @@ function SignUp() {
   }, []);
 
   const submitForm = (formValues) => {
-    return dispatch(createCandidates(formValues));
+    dispatch(createCandidates(formValues));
   };
 
   const validate = (formValues) => {
     const errors = {};
+    errors.firstName = validateText(formValues.firstName, 'First Name', 2, 40);
+    errors.lastName = validateText(formValues.lastName, 'Last Name', 2, 40);
+    errors.phone = validatePhone(formValues.phone, 'Phone Number');
     errors.email = validateEmail(formValues.email);
     errors.password = validateText(formValues.password, 'Password', 8, 16);
+    errors.city = validateText(formValues.city, 'City', 2, 40);
+    errors.province = validateText(formValues.province, 'Province', 2, 40);
+    errors.country = validateText(formValues.country, 'Country', 2, 40);
+    errors.postalCode = validateZipCode(formValues.postalCode);
+    errors.birthday = birthdayValidation(formValues.birthday);
+    errors.address = {
+      street: validateText(formValues.address?.street, 'Street', 2),
+      number: validateText(formValues.address?.number, 'Number')
+    };
+
     return errors;
   };
 
   return (
     <>
-      <section className={styles.container}>
-        <h1 className={styles.mainTitle}>Sign Up</h1>
-        <Form
-          onSubmit={submitForm}
-          initialValues={formData}
-          validate={validate}
-          subscription={{
-            submitting: true
-          }}
-          render={({ handleSubmit, submitting, pristine }) => (
-            <form className={styles.form} onSubmit={handleSubmit}>
-              <Field name="email" label="Email" element="input" type="email" component={Fieldset} />
-              <Field
-                name="password"
-                label="Password"
-                element="input"
-                type="password"
-                component={Fieldset}
-              />
-              <Field
-                name="password"
-                label="Confirm Password"
-                element="input"
-                type="password"
-                component={Fieldset}
-              />
-              <div className={styles.btnContainer}>
-                <button
-                  className={`${styles.buttonGreen} ${(submitting || pristine) && styles.disabled}`}
-                  type="submit"
-                  disabled={submitting || pristine}
-                >
-                  Continue
-                </button>
-              </div>
-            </form>
-          )}
-        />
-      </section>
-      {modal && <Modal />}
+      <body>
+        <section className={styles.container}>
+          <h1 className={styles.mainTitle}>{FormTitle[page]}</h1>
+          {PageDisplay()}
+          <button
+            className={styles.buttonGreen}
+            disabled={page === 0}
+            onClick={() => {
+              setPage((currPage) => currPage - 1);
+            }}
+          >
+            Prev
+          </button>
+          <button
+            className={styles.buttonGreen}
+            disabled={page === FormTitle.length - 1}
+            onClick={() => {
+              setPage((currPage) => currPage + 1);
+            }}
+          >
+            Continue
+          </button>
+        </section>
+      </body>
     </>
   );
 }
---
+++ src/Components/Candidate/Signup/signup1.jsx
@@ -0,0 +1,69 @@
+import { useEffect } from 'react';
+import Fieldset from 'Components/shared/Fieldset';
+import styles from './signup.module.css';
+import Modal from 'Components/shared/Modal';
+import { useDispatch, useSelector } from 'react-redux';
+import { createCandidates } from 'redux/admin/candidates/thunks';
+import { candidatesCleanUp } from 'redux/admin/candidates/actions';
+import { Form, Field } from 'react-final-form';
+import { validateText, validateEmail } from 'validations';
+
+function SignUp1() {
+  const dispatch = useDispatch();
+  const formData = useSelector((store) => store.candidates.selectedElement);
+  const modal = useSelector((store) => store.modal.show);
+
+  useEffect(() => {
+    return () => {
+      dispatch(candidatesCleanUp());
+    };
+  }, []);
+
+  const submitForm = (formValues) => {
+    return dispatch(createCandidates(formValues));
+  };
+
+  const validate = (formValues) => {
+    const errors = {};
+    errors.email = validateEmail(formValues.email);
+    errors.password = validateText(formValues.password, 'Password', 8, 16);
+    return errors;
+  };
+
+  return (
+    <>
+      <section className={styles.container}>
+        <Form
+          onSubmit={submitForm}
+          initialValues={formData}
+          validate={validate}
+          subscription={{
+            submitting: true
+          }}
+          render={({ handleSubmit, submitting, pristine }) => (
+            <form className={styles.form} onSubmit={handleSubmit}>
+              <Field name="email" label="Email" element="input" type="email" component={Fieldset} />
+              <Field
+                name="password"
+                label="Password"
+                element="input"
+                type="password"
+                component={Fieldset}
+              />
+              <Field
+                name="password"
+                label="Password"
+                element="input"
+                type="password"
+                component={Fieldset}
+              />
+            </form>
+          )}
+        />
+      </section>
+      {modal && <Modal />}
+    </>
+  );
+}
+
+export default SignUp1;
