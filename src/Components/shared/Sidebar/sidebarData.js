da7e56a2f6e2f905ea67084b76a34f75eaef1334
Sabrina <pereira.tsabrina@gmail.com>
2022-01-01 14:44:55 -0300

    MS-45: psychologist routes in sidebar added

--- src/Components/shared/Sidebar/sidebarData.js
+++ src/Components/shared/Sidebar/sidebarData.js
@@ -61,62 +61,16 @@ const sidebarCandidate = [
 
 const sidebarPsychologist = [
   {
-    title: 'Curriculum Vitae',
-    path: '#',
-    subNav: [
-      {
-        title: 'Personal Information',
-        path: '/candidate/curriculumvitae/personal-information',
-        cName: 'nav'
-      },
-      {
-        title: 'Basic Education',
-        path: '/candidate/curriculumvitae/basic-education',
-        cName: 'nav'
-      },
-      {
-        title: 'College Education & Plus',
-        path: '/candidate/curriculumvitae/college-education',
-        cName: 'nav'
-      },
-      {
-        title: 'Other Education',
-        path: '/candidate/curriculumvitae/other-education',
-        cName: 'nav'
-      },
-      {
-        title: 'Work Experience',
-        path: '/candidate/curriculumvitae/work-experience',
-        cName: 'nav'
-      },
-      {
-        title: 'Hobbies & Skills',
-        path: '/candidate/curriculumvitae/hobbies-and-skills',
-        cName: 'nav'
-      },
-      {
-        title: 'Time Range',
-        path: '/candidate/curriculumvitae/time-range',
-        cName: 'nav'
-      },
-      {
-        title: 'Work Profile',
-        path: '/candidate/curriculumvitae/work-profile',
-        cName: 'nav'
-      }
-    ]
+    title: 'Personal Schedule',
+    path: '/psychologist/personal-schedule'
   },
   {
-    title: 'Job Interviews',
-    path: '/candidate/profile/job-interviews'
+    title: 'List of appointments',
+    path: '/psychologist/list-of-appointments'
   },
   {
-    title: 'Availability',
-    path: '/candidate/profile/availability'
-  },
-  {
-    title: 'Job Oportunities',
-    path: '/candidate/profile/job-oportunities'
+    title: 'Appointments Results',
+    path: '/psychologist/appointments-results'
   }
 ];
 
